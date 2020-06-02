9e15800d2195f7dd4317d2226bc550f7ec1e03ff
Marcelo <mparajo@gmail.com>
2020-06-02 17:29:56 -0300

    for checkout

--- resources/views/home/dispositions/caja-general.blade.php
+++ resources/views/home/dispositions/caja-general.blade.php
@@ -51,7 +51,7 @@
 					<div class="notas-cajageneral perfil-educacion">
 
 						<div class="header-cajageneral">
-							<h6>{{ $news[0]['hat'] ?? '' }}</h6>
+							<h6>Perfil Educación</h6>
 						</div>
 
 						<div class="columna-a"> 
@@ -90,6 +90,116 @@
 						</div>
 
 					</div><!-- notas-cobertura -->
+
+
+
+
+
+
+
+
+					<div class="notas-cajageneral marieclaire">
+
+						<div class="header-cajageneral">
+							<h6>Marie Claire</h6>
+						</div>
+
+						<div class="columna-a"> 
+							@foreach(array_slice($news, 0, 3) as $key => $new)
+							<article class="caja-nota">
+								<a href="{{ $new['permalink'] }}">
+									<figure>
+										{!! $imageHelper->getLazyImages( $new['main_image']['srcs']['medium-wide'], 540, $new['main_image']['caption'],'img-fluid','540x304') !!}
+									</figure>
+
+									<div class="meta-content">
+										@if ($new['hat'] != '')
+											<h3 class="hat">{{ $new['hat'] }} </h3>
+										@endif
+										<h2>
+											{{ $new['home_title'] }}
+										</h2>
+										<h4>{{ $new['headline'] }}</h4>
+										@if ($new['signed'])
+											<h5 class="firma-home">{{ __('by') }} {{ $new['author']['fullname'] }}</h5>
+										@endif
+									</div>
+								</a>
+							</article>
+							@endforeach
+						</div>
+
+						
+						<div class="footer-cajageneral">
+							<ul>
+								<li><a href="//marieclaire.perfil.com/seccion/gourmet">Gourmet</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/cultura">Cultura</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/sociedad">Sociedad</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/personajes">Personajes</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/moda">Moda</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/lifestyle">Lifestyle</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/belleza">Belleza</a></li>
+							</ul>
+						</div>
+
+					</div><!-- notas-cobertura -->
+
+
+
+
+
+
+
+
+					<div class="notas-cajageneral canalnet">
+
+						<div class="header-cajageneral">
+							<h6>Canal Net</h6>
+						</div>
+
+						<div class="columna-a"> 
+							@foreach(array_slice($news, 0, 3) as $key => $new)
+							<article class="caja-nota">
+								<a href="{{ $new['permalink'] }}">
+									<figure>
+										{!! $imageHelper->getLazyImages( $new['main_image']['srcs']['medium-wide'], 540, $new['main_image']['caption'],'img-fluid','540x304') !!}
+									</figure>
+
+									<div class="meta-content">
+										@if ($new['hat'] != '')
+											<h3 class="hat">{{ $new['hat'] }} </h3>
+										@endif
+										<h2>
+											{{ $new['home_title'] }}
+										</h2>
+										<h4>{{ $new['headline'] }}</h4>
+										@if ($new['signed'])
+											<h5 class="firma-home">{{ __('by') }} {{ $new['author']['fullname'] }}</h5>
+										@endif
+									</div>
+								</a>
+							</article>
+							@endforeach
+						</div>
+
+						
+						<div class="footer-cajageneral">
+							<ul>
+								<li><a href="//marieclaire.perfil.com/seccion/sociedad">Tecnología</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/personajes">Tendencias</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/moda">Espectáculos</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/lifestyle">Deportes</a></li>
+								<li><a href="//marieclaire.perfil.com/seccion/belleza">Noticias</a></li>
+							</ul>
+						</div>
+
+					</div><!-- notas-cobertura -->
+
+
+
+
+
+
 
 
 				</div>
