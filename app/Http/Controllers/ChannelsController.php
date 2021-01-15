3f43506b157dae131c1399a21425a39f06810a91
Federico Ramato <federicoramato@MacBook-Pro-de-Federico.local>
2021-01-15 13:57:36 -0300

    Controller para columnistas

--- app/Http/Controllers/ChannelsController.php
+++ app/Http/Controllers/ChannelsController.php
@@ -11,10 +11,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Helpers\ApiHelper;
 use App\Http\Helpers\BloquesHelper;
+use App\Http\Helpers\ImageHelper;
 use App\Http\Helpers\ParseHelper;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Routing\Route;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
@@ -89,6 +92,50 @@ class ChannelsController extends Controller
         return view('channels.index', compact('channel', 'noticias', 'sectionTitle', 'sidebar_content', 'page_description', 'analytics_data', 'amphtml'));
     }
 
+    public function showColumnistas(ImageHelper $imageHelper)
+    {
+        $payload = $this->apiHelper->getColumnistas();
+        $content = [];
+
+        $author_piority_order = ['jfontevecchia', 'ncastro', 'jduranbarba', 'ggonzalez', 'jcalvo', 'qquintin', 'jpetrarca'];
+
+        // Recorro el contenido y lo agrupo por autor.
+        foreach ($payload->DATA as $item) {
+            if (! isset($content[$item['author_username']])) {
+                $position = array_search($item['author_username'], $author_piority_order);
+
+                $content[$item['author_username']] = [
+                    'username' => $item['author_username'],
+                    'first_name' => $item['author_firstname'],
+                    'last_name' => $item['author_lastname'],
+                    'slug' => $item['author_slug'],
+                    'twitter' => $item['author_twitter'],
+                    'facebook' => $item['author_facebook'],
+                    'instagram' => $item['author_instagram'],
+                    'google_plus' => $item['author_googleplus'],
+                    'site' => $item['author_blogsite'],
+                    'about' => $item['author_about'],
+                    'image' => $imageHelper->generateUrlImageAuthor($item['author_username'], 50),
+                    'position' => ($position !== false) ? $position + 1 : 999,
+                    'posts' => []
+                ];
+            }
+
+            array_push($content[$item['author_username']]['posts'], $this->parseHelper->parseNoticia($item));
+        }
+
+        $authors = array_values(Arr::sort($content, function($value) {
+            return $value['position'];
+        }));
+
+        dd($authors);
+    }
+
+    /**
+     * @param Route $route
+     * @param BloquesHelper $bloquesHelper
+     * @return Application|Factory|View
+     */
     public function amp(Route $route, BloquesHelper $bloquesHelper){
         $channel = $route->parameter('channel');
         $payload = $this->apiHelper->getNewsFromChannel($channel);
@@ -120,6 +167,11 @@ class ChannelsController extends Controller
         return view('amp.lists', compact('channel', 'noticias', 'sectionTitle', 'page_description', 'analytics_data', 'canonical'));
     }
 
+    /**
+     * @param Route $route
+     * @return Application|Factory|View
+     * @throws FileNotFoundException
+     */
     public function showCustomizable(Route $route)
     {
         $subchannels_list = [
