a38751b71e8fe9d8ca705f2324df2e6be4ff75ed
Federico Ramato <federicoramato@MacBook-Pro-de-Federico.local>
2021-02-12 05:48:15 -0300

    Corrige datos del footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -12,7 +12,7 @@
 		<a href="https://www.youtube.com/channel/{{ env('YOUTUBE_CHANNEL', '') }}" target="_blank" class="link-social-youtube" itemprop="sameAs" rel="noreferrer"><img src="/images/glyph/footer/youtube.svg"  style="width:20px;height:14px" alt="Seguinos en Youtube" /></a>
 		<a href="https://www.instagram.com/{{ env('INSTAGRAM_PAGE', '') }}/" target="_blank" class="link-social-instagram" itemprop="sameAs" rel="noreferrer"><img src="/images/glyph/footer/instagram.svg" alt="Seguinos en Instagram"  style="width:20px;height:20px" /></a>
 		<a href="{{ route('feeds.channel', '') }}" target="_blank" class="link-social-rss" rel="noreferrer"><img src="/images/glyph/footer/rss.svg"  alt="RSS"  style="width:17px;height:17px" /></a>
-		<a href="https://www.{{ env('APP_NAME', '') }}.com.ar/sitemap" target="_blank" class="link-social-sitemap" rel="noreferrer"><img src="/images/glyph/footer/sitemap.svg"  style="width:20px;height:15px" alt="Sitemap" /></a>
+		<a href="{{ route('sitemaps.index') }}" target="_blank" class="link-social-sitemap" rel="noreferrer"><img src="/images/glyph/footer/sitemap.svg"  style="width:20px;height:15px" alt="Sitemap" /></a>
 	</div>
 	<nav class="nav-footer">
 		<ul class="footer-revistas">
@@ -22,7 +22,7 @@
 		</ul>
 	</nav>
 	<div class="disclaimer">
-		<span itemprop="name">{{ strtolower(env('APP_NAME')) }}.perfil.com - Editorial Perfil S.A.</span> | © Perfil.com 2006-{{ date('Y') }} - All rights reserved<br />
+		<span itemprop="name">Perfil.com - Editorial Perfil S.A.</span> | © Perfil.com 2006-{{ date('Y') }} - All rights reserved<br />
 		Intellectual Property Registry Number 5346433<br />
 		<div class="d-none d-xl-block" itemprop="address" itemscope itemtype="https://schema.org/PostalAddress">
 		Address: <span itemprop="streetAddress">California 2715</span>, <span itemprop="postalCode">C1289ABI</span>, <span itemprop="addressLocality">CABA, Argentina</span>  | Phone: <span itemprop="telephone">(+5411) 7091-4921</span> / <span itemprop="telephone">(+5411) 7091-4922</span> | E-mail: <a href="mailto:perfilcom@perfil.com"><span itemprop="email">perfilcom@perfil.com</span></a>
