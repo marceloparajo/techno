84e927f4340fd689ef9f903ed8120ec8dc1b0bb0
Federico Ramato <federicoramato@MacBook-Pro-de-Federico.local>
2021-02-23 02:39:00 -0300

    Menu footer

--- routes/web/pages.php
+++ routes/web/pages.php
@@ -1,11 +1,11 @@
 <?php
 
-Route::view('canales-rss', 'pages.canales-rss');
+Route::view('canales-rss', 'pages.canales-rss')->name('pages.canales.rss');
 
-Route::view('terminos-privacidad', 'pages.tyc');
+Route::view('terminos-privacidad', 'pages.tyc')->name('pages.tyc');
 
-Route::view('staff', 'pages.staff');
+Route::view('staff', 'pages.staff')->name('pages.staff');
 
-Route::view('reglas-participacion', 'pages.reglas-participacion');
+Route::view('reglas-participacion', 'pages.reglas-participacion')->name('pages.reglas.participacion');
 
-Route::view('transito', 'pages.transito');
+Route::view('transito', 'pages.transito')->name('pages.transito');
