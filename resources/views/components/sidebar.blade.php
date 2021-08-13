fd0732aeb8d9efbe069b4e4036aed762ca2abdea
Marcelo <mparajo@gmail.com>
2021-08-13 14:07:43 -0300

    agregue salud y astrologia en sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -14,24 +14,8 @@
                 <x-mas-leidas-perfil-widget :rows="5" />
                 @break
 
-            @case('columnistas')
-                <x-columnistas-widget :news="$item['news']" />
-                @break
-
             @case('minuto-de-noticias')
                 <x-minuto-de-noticias-widget />
-                @break
-
-            @case('batimes')
-                <x-batimes-widget />
-                @break
-
-            @case('ventas-directas')
-                <div class="ventas-directas">
-                    <a href="{{ route('channels.show', 'guia-de-salud-pnt') }}"><img src="{{ asset('images/ventas/guia-salud.png') }}" style="margin:10px 0;border:none;width:300px;height:50px;" alt="guia de la salud"></a>
-
-                    <a href="{{ route('channels.show', 'guia-de-profesionales-pnt') }}"><img src="{{ asset('images/ventas/guia-profesionales.png') }}" style="margin:10px 0;border:none;width:300px;height:50px" alt="guia de profesionales"></a>
-                </div>
                 @break
 
             @default
@@ -39,11 +23,29 @@
                     @include('sidebar.dispositions.' . $item['template'], ['news' => $item['news']])
                 @endif
 
-        @endswitch
-
-        @if ($loop->iteration % 2 && $loop->iteration < 11)
-            <x-ad-space id="sidebar_300x250x-pos-" width="300" height="250" />
-        @endif
+        @endswitch 
+        
+        {{--
+            @if ($loop->iteration % 2 && $loop->iteration < 11)
+                <x-ad-space id="sidebar_300x250x-pos-" width="300" height="250" />
+            @endif
+        --}}
 
     @endforeach
+
+    <div class="placa-home">
+        <a href="/seccion/salud" title="Caras Salud" rel="noreferrer">
+            <img src="{{ asset('images/caras_salud.png') }}" alt="{{ env('APP_NAME') }}">
+        </a>
+    </div>
+
+    <x-ad-space id="sidebar_300x250x-pos-" width="300" height="250" margin-top="0" max-height="600" />
+
+    <div class="placa-home">
+        <a href="/seccion/horoscopo" title="Caras Horoscopo" rel="noreferrer">
+            <img src="{{ asset('images/caras_astrologia.png') }}" alt="{{ env('APP_NAME') }}">
+        </a>
+    </div>
+
+
 </div>
