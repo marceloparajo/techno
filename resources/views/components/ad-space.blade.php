93fc3c802d99f3b8c066a0ae811f066ad63eaf9a
Marcelo <mparajo@gmail.com>
2021-12-07 14:08:32 -0300

    correcciones css despues de integrar el componente x-image

--- resources/views/components/ad-space.blade.php
+++ resources/views/components/ad-space.blade.php
@@ -1,5 +1,5 @@
 <div style="width: {{ $width }}px; 
-    @if (! is_null($max_height)) max-height: {{ $max_height }}px; @endif min-height: {{ $min_height }}px; display: flex; flex-direction: column; align-items: center; margin-top: {{ $margin_top }}px; margin-bottom: {{ $margin_bottom }}px; overflow: hidden;" class="{{ $class }}">
+    @if (! is_null($max_height)) max-height: {{ $max_height }}px; @endif min-height: {{ $min_height }}px; display: flex; flex-direction: column; align-items: center; margin-top: {{ $margin_top }}px; margin-bottom: {{ $margin_bottom }}px; overflow: hidden;" class="{{ $class }};margin-left: auto;margin-right: auto;">
     <div id="" class="ads-space {{ $class }}" data-id="{{ $id }}" data-w="{{ $width }}" data-h="{{ $height }}" data-loaded="false" data-reload="{{ $reload }}"></div>
 </div>
 
