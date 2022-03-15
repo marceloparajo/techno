cde5a322e489a9ddc10d3fdf21ce1fa7ffc52a0b
Juli Staheli <julistaheli@gmail.com>
2022-03-15 16:17:43 -0300

    lazy load false en destacadas

--- resources/views/home/dispositions/destacada.blade.php
+++ resources/views/home/dispositions/destacada.blade.php
@@ -11,7 +11,7 @@
 					@else
 						<figure>
 							@if($loop->first)
-								<x-lazy-image
+								<x-lazy-image :lazy-load="false"
 										:src="$new['main_image']['srcs']['original']"
 										:alt="$new['main_image']['title']"
 										:sizes="[['v' => 320, 'w' => 540, 'h' => 324], ['v' => 768, 'w' => 728, 'h' => 410], ['v' => 1024, 'w' => 720, 'h' => 405]]"
@@ -20,7 +20,7 @@
 										:play-button="$new['has_video']"
 										:camera-button="$new['has_gallery']" />
 							@else
-								<x-lazy-image
+								<x-lazy-image :lazy-load="false"
 										:src="$new['main_image']['srcs']['original']"
 										:alt="$new['main_image']['title']"
 										:sizes="[['v' => 320, 'w' => 540, 'h' => 324], ['v' => 768, 'w' => 350, 'h' => 210], ['v' => 1024, 'w' => 252, 'h' => 151], ['v' => 1366, 'w' => 300, 'h' => 180]]"
