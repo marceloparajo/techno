e597f240c39142fb30c3f815025f9f098d45183d
Whopa <hola@whopa.ar>
2022-05-24 01:31:10 -0300

    Fix relacionadas

--- resources/views/news/show/partials/noticias-relacionadas.blade.php
+++ resources/views/news/show/partials/noticias-relacionadas.blade.php
@@ -6,7 +6,8 @@
                 <a href="{{ $new['permalink'] }}">
                     <figure>
                         <x-lazy-image
-                            :src="$new['image']['srcs']['original']"
+                            :src="$new['image']['srcs']['small']"
+                            clean-source="true"
                             :alt="$new['image']['title']"
                             :sizes="[['v' => 320, 'w' => 121, 'h' => 68], ['v' => 360, 'w' => 138, 'h' => 77], ['v' => 375, 'w' => 143, 'h' => 80], ['v' => 414, 'w' => 160, 'h' => 90], ['v' => 768, 'w' => 163, 'h' => 94], ['v' => 1024, 'w' => 144, 'h' => 80],['v' => 1366, 'w' => 206, 'h' => 115]]"
                             class="img-fluid" />
