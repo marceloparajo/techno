8117e4d78563368d91389d71ed8875545fd84cba
mparajo <mparajo@gmail.com>
2023-03-17 13:17:51 -0300

    columna notas chicas sticky en xl

--- resources/views/home/dispositions/bloque.blade.php
+++ resources/views/home/dispositions/bloque.blade.php
@@ -2,28 +2,28 @@
 	<!-- Bloque -->
 	<div class="row max-width bloque">
 
-	<div class="col-fluid d-xl-flex bloque__container">
+		<div class="col-fluid d-xl-flex bloque__container">
 
 
-		@if ( $item['id'] == 'bloque-superior' )
-			<x-divisas-widget />
-		@endif
+			@if ( $item['id'] == 'bloque-superior' )
+				<x-divisas-widget />
+			@endif
 
-		<div class="col-fluid flex-md-doble">
-		@foreach(array_slice($news, 0, 6) as $new)
-			@include('partials.articulo', array('clase' => 'news--card-rounded', 'width_mobile' => '375', 'height_mobile' => '211', 'width_tablet' => '367', 'height_tablet' => '220', 'width_laptop' => '309', 'height_laptop' => '174', 'width_desktop' => '423', 'height_desktop' => '238'))
-		@endforeach
-		</div>
-		<div class="col-xl-fixed flex-md-doble flex-xl-single">
-			<div class="row">
-				<x-ad-space id="300x250x-pos-" width="300" height="250" min-height="250" max-height="250" class="ads" /> 
-			</div>
-			@foreach(array_slice($news, 6, 4) as $new)
-				@include('partials.articulo', array('clase' => 'news--small-picture news--border-bottom', 'width_mobile' => '160', 'height_mobile' => '160', 'width_tablet' => '160', 'height_tablet' => '160', 'width_laptop' => '160', 'height_laptop' => '160', 'width_desktop' => '160', 'height_desktop' => '160'))
+			<div class="col-fluid flex-md-doble">
+			@foreach(array_slice($news, 0, 6) as $new)
+				@include('partials.articulo', array('clase' => 'news--card-rounded', 'width_mobile' => '375', 'height_mobile' => '211', 'width_tablet' => '367', 'height_tablet' => '220', 'width_laptop' => '309', 'height_laptop' => '174', 'width_desktop' => '423', 'height_desktop' => '238'))
 			@endforeach
-		</div>
+			</div>
+			<div class="col-xl-fixed flex-md-doble flex-xl-single col-xl-sticky">
+				<div class="row">
+					<x-ad-space id="300x250x-pos-" width="300" height="250" min-height="250" max-height="250" class="ads" /> 
+				</div>
+				@foreach(array_slice($news, 6, 4) as $new)
+					@include('partials.articulo', array('clase' => 'news--small-picture news--border-bottom', 'width_mobile' => '160', 'height_mobile' => '160', 'width_tablet' => '160', 'height_tablet' => '160', 'width_laptop' => '160', 'height_laptop' => '160', 'width_desktop' => '160', 'height_desktop' => '160'))
+				@endforeach
+			</div>
 
-	</div>
+		</div>
 
 		<div class="col-fixed">
 
